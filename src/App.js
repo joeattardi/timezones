4f6863f1a4a0a194b57f8a8ee3beff3eecb93f9c
Joe Attardi <jattardi@gmail.com>
2020-09-05 22:05:32 -0400

    Add time picker

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { format, parse } from 'date-fns';
 import { zonedTimeToUtc, utcToZonedTime } from 'date-fns-tz';
 
 import Select from 'react-select';
+import TimePicker from 'react-time-picker';
 
 import Header from './Header';
-import Input from './Input';
 
 const timeZones = getTimeZones().sort((a, b) => a.name.localeCompare(b.name));
 const tzOptions = timeZones.map(zone => ({ value: zone, label: zone.name }));
@@ -18,15 +18,15 @@ const currentTimeZoneValue = { value: currentTimeZone, label: currentTimeZone.na
 
 export default function App() {
   const [sourceTimeZone, setSourceTimeZone] = useState(currentTimeZoneValue);
-  const [sourceTime, setSourceTime] = useState(format(new Date(), 'h:mm a'));
+  const [sourceTime, setSourceTime] = useState(format(new Date(), 'H:mm'));
 
   const [destinationTimeZone, setDestinationTimeZone] = useState();
   const [destinationTime, setDestinationTime] = useState('');
 
   useEffect(doTimeConversion, [sourceTimeZone, sourceTime, destinationTimeZone]);
 
-  function updateSourceTime(event) {
-    setSourceTime(event.target.value);
+  function updateSourceTime(time) {
+    setSourceTime(time);
   }
 
   function updateSourceTimeZone(value) {
@@ -39,7 +39,7 @@ export default function App() {
 
   function doTimeConversion() {
     if (sourceTimeZone && sourceTime && destinationTimeZone) {
-      const sourceTimeObj = parse(`${sourceTime}`, 'h:mm a', new Date());
+      const sourceTimeObj = parse(`${sourceTime}`, 'H:mm', new Date());
       
       if (isNaN(sourceTimeObj.getTime())) {
         setDestinationTime('');
@@ -53,32 +53,33 @@ export default function App() {
   return (
     <div>
       <Header />
-      <main className="p-2 flex flex-col sm:flex-row">
-        <section className="flex-grow">
-          <h2 className="text-xl font-bold">Source Time</h2>
-          <div className="flex flex-col">
-            <label>Time zone</label>
-            <Select options={tzOptions} value={sourceTimeZone} onChange={updateSourceTimeZone} />
-          </div>
+      <main>
+        <div className="p-2 flex flex-col sm:flex-row">
+          <section className="flex-grow">
+            <h2 className="text-xl font-bold">Source Time</h2>
+            <div className="flex flex-col">
+              <label>Time zone</label>
+              <Select options={tzOptions} value={sourceTimeZone} onChange={updateSourceTimeZone} />
+            </div>
 
-          <div className="flex flex-col">
+            <div className="flex flex-col">
+              <label>Time</label>
+              <TimePicker value={sourceTime} onChange={updateSourceTime} disableClock={true} clearIcon={null} />
+            </div>
+          </section>
+          
+          <section className="mt-4 sm:mt-0 sm:ml-4 flex-grow">
+            <h2 className="text-xl font-bold">Destination Time</h2>
+            <div className="flex flex-col">
+              <label>Time zone</label>
+              <Select options={tzOptions} value={destinationTimeZone} onChange={updateDestinationTimeZone} />
+            </div>
+          </section>
+        </div>
+          <div className="flex flex-col p-2">
             <label>Time</label>
-            <Input value={sourceTime} onChange={updateSourceTime} />
+            <div className="font-bold text-4xl">{destinationTime}</div>
           </div>
-        </section>
-        
-        <section className="mt-4 sm:mt-0 sm:ml-4 flex-grow">
-          <h2 className="text-xl font-bold">Destination Time</h2>
-          <div className="flex flex-col">
-            <label>Time zone</label>
-            <Select options={tzOptions} value={destinationTimeZone} onChange={updateDestinationTimeZone} />
-          </div>
-
-          <div className="flex flex-col">
-            <label>Time</label>
-            <div className="font-bold text-2xl">{destinationTime}</div>
-          </div>
-        </section>
       </main>
     </div>
   );
